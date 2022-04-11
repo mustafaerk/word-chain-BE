5b0bd8a2004873e847ba940fe9d15d6654fdb826
mustafaerk <telemak33@gmail.com>
2022-04-11 22:40:34 +0300

    Jwt token verify added. Routes undercontrol, nodemon added to npm start script

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
 /* DB MODELS */
-const RoomModel = require("./db/roomSchema");
 
 /* ---------------------------------- */
 
 /* Imports */
-const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const authRoutes = require('./src/route/authRoutes');
+const roomRoutes = require('./src/route/roomRoutes');
+
 /* ---------------------------------- */
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URL || 'mongodb://localhost:27017/wordChainDev';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,100 +38,15 @@ app.use((req, res, next) => {
 });
 
 /* DB connection */
-const uri = process.env.MONGO_URL;
 console.log(uri);
 mongoose.connect(uri, {
   useNewUrlParser: true,
 });
 /* ---------------------------------- */
 
-app.post("/createRoom", (req, res) => {
-  console.log(req.body);
-  const { roomAvatarId, userInfo, roomSize, roomName, isPublic } = req.body;
 
-  const room = {
-    roomId: uuidv4(),
-    createDate: Date.now(),
-    roomAvatarId,
-    ownerId: userInfo.id,
-    users: [
-      {
-        name: userInfo.name,
-        id: userInfo.id,
-        isEliminated: false,
-        language: userInfo.lang,
-        userAvatarId: userInfo.avatarId,
-      },
-    ],
-    roomSize,
-    roomName,
-    isPublic,
-  };
-  RoomModel.create(room, (err) => {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    }
-    res.statusCode = 200;
-    res.statusMessage = "Success";
-    res.send({ status: res.statusCode, message: res.statusMessage });
-  });
-});
-
-app.get("/listRooms/", (req, res) => {
-  RoomModel.find({ isActive: true, isPublic: true }, (err, rooms) => {
-    var roomMap = {};
-    rooms.forEach((room) => {
-      roomMap = {
-        roomId: room.roomId,
-        roomAvatarId: room.roomAvatarId,
-        roomName: room.roomName,
-        roomStatus: room.isStarted,
-        roomSize: room.roomSize,
-      };
-    });
-    res.send(roomMap);
-  });
-});
-
-app.post("/joinRoom", function (req, res) {
-  const { roomId, userInfo } = req.body;
-
-  RoomModel.findOne({ roomId: roomId }, (err, room) => {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    }
-
-    var user = {
-      id: userInfo.id,
-      name: userInfo.name,
-      isEliminated: false,
-      language: userInfo.lang,
-      userAvatarId: userInfo.avatarId,
-    };
-    const searchFindUser = (element) => element.id == user.id;
-    if (room.blockedUsers.findIndex(searchFindUser) != -1) {
-      res.send("User blocked.");
-      return;
-    }
-    console.log(room.roomSize);
-    console.log(room.users.length);
-    if (room.users.length < room.roomSize) {
-      room.users.push(user);
-      room.save((err, response) => {
-        if (err) res.send(err);
-        res.statusCode = 200;
-        res.statusMessage = "Success";
-        res.send({ status: res.statusCode, message: res.statusMessage });
-      });
-    } else {
-      res.statusCode = 400;
-      res.statusMessage = "Failed";
-      res.send({ status: res.statusCode, message: res.statusMessage });
-    }
-  });
-});
+app.use(authRoutes);
+app.use(roomRoutes);
 
 app.listen(PORT, () => {
   console.log(PORT, "Sunucu çalışıyor...");
---
+++ src/middleware/jwtVerify.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const jwtKey = process.env.JWT_TOKEN_PUBLIC;
+
+module.exports = (req, res, next) => {
+    try {
+        /*JWT is send with request header! 
+        Format of it: Authorization : Bearer <token>
+        */
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, jwtKey);
+        req.body.userToken = decodedToken;
+        next();
+    } catch (error) {
+        return res.status(401).send({
+            message: 'Auth failed',
+            error
+        });
+    }
+}
