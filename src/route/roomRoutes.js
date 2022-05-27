3812c1a3174260387b026f774528ce63b3990c44
mustafaerk <telemak33@gmail.com>
2022-05-27 19:50:23 +0300

    routing change

--- src/route/roomRoutes.js
+++ src/route/roomRoutes.js
@@ -1,17 +1,16 @@
 const { Router } = require('express');
 const roomController = require('../controllers/roomController');
-const verifyJwt = require('../middleware/jwtVerify');
 
 const router = Router();
 
-router.post('/createRoom', verifyJwt, roomController.createRoom_post);
-router.get('/listRooms', verifyJwt, roomController.listRoom_get);
-router.post('/joinRoom', verifyJwt, roomController.joinRoom_post);
-router.post('/leaveRoom', verifyJwt, roomController.leaveRoom_post);
-router.post('/quickJoin', verifyJwt, roomController.quickjoin_post);
-router.post('/startGame', verifyJwt, roomController.startGame_post);
-router.post('/timeUp', verifyJwt, roomController.timeUp_post);
-router.post('/restartGame', verifyJwt, roomController.restartGame_post);
+router.post('/createRoom', roomController.createRoom_post);
+router.get('/listRooms', roomController.listRoom_get);
+router.post('/joinRoom', roomController.joinRoom_post);
+router.post('/leaveRoom', roomController.leaveRoom_post);
+router.post('/quickJoin', roomController.quickjoin_post);
+router.post('/startGame', roomController.startGame_post);
+router.post('/timeUp', roomController.timeUp_post);
+router.post('/restartGame', roomController.restartGame_post);
 
 module.exports = router;
 
